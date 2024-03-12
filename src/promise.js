7e2ac25d6e8a288a253acbaaae16928d1f1952e3
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-12 15:52:55 +0800

    GREEN: MyPromise › should resolve with a value

--- src/promise.js
+++ src/promise.js
@@ -1,5 +1,21 @@
-function MyPromise() {
-    this.then = () => {};
+function MyPromise(executor = () => {}) {
+    this.then = (onResolvedFunction) => {
+        this.onResolvedFunction = onResolvedFunction;
+
+        if (this.result !== undefined) {
+            onResolvedFunction(this.result);
+        }
+    };
+
+    this.resolveFunction = (result) => {
+        this.result = result;
+
+        if (this.onResolvedFunction !== undefined) {
+            this.onResolvedFunction(result);
+        }
+    };
+
+    executor(this.resolveFunction);
 }
 
 export default MyPromise;
