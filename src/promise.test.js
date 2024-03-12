fb06aca0974cfc34073d75e7da8f9a2c2b86c8d5
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-12 15:50:05 +0800

    RED: MyPromise › should have a then method

--- src/promise.test.js
+++ src/promise.test.js
@@ -4,4 +4,9 @@ describe('MyPromise', () => {
     it('should be a constructor', () => {
         expect(typeof MyPromise).toBe('function');
     });
+    it('should have a then method', () => {
+        const myPromise = new MyPromise();
+
+        expect(typeof myPromise.then).toBe('function');
+    });
 });
