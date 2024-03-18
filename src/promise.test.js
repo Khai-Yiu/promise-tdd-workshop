8adcbfb426473f0c24900faa7776d9eae8a3d588
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-18 15:20:33 +0800

    RED/GREEN:  MyPromise › given a promise that resolves after some time › and the callback is registered before it resolves › calls the registered callback when it resolves

--- src/promise.test.js
+++ src/promise.test.js
@@ -1,4 +1,6 @@
 import MyPromise from './promise';
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
 
 describe('MyPromise', () => {
     it('should be a constructor', () => {
@@ -9,14 +11,53 @@ describe('MyPromise', () => {
 
         expect(typeof myPromise.then).toBe('function');
     });
-    it('should resolve with a value', () => {
-        const executor = (resolveFunction) => {
-            resolveFunction(1);
-        };
-        const myPromise = new MyPromise(executor);
-        const functionToCallWhenPromiseIsResolved = jest.fn();
-        myPromise.then(functionToCallWhenPromiseIsResolved);
+    describe('given a promise that resolves immediately', () => {
+        it('should resolve with a value', () => {
+            const executor = (resolveFunction) => {
+                resolveFunction(1);
+            };
+            const myPromise = new MyPromise(executor);
+            const functionToCallWhenPromiseIsResolved = jest.fn();
+            myPromise.then(functionToCallWhenPromiseIsResolved);
 
-        expect(functionToCallWhenPromiseIsResolved).toHaveBeenCalledWith(1);
+            expect(functionToCallWhenPromiseIsResolved).toHaveBeenCalledWith(1);
+        });
+    });
+    describe('given a promise that resolves after some time', () => {
+        beforeAll(() => {
+            jest.useFakeTimers();
+        });
+
+        afterAll(() => {
+            jest.useRealTimers();
+        });
+        describe('and the callback is registered before it resolves', () => {
+            it('calls the registered callback when it resolves', () => {
+                const executor = (resolveFunction) => {
+                    setTimeout(() => {
+                        resolveFunction(1);
+                    }, 1000);
+                };
+                const myPromise = new MyPromise(executor);
+                const functionToCallWhenPromiseIsResolved = jest.fn();
+                myPromise.then(functionToCallWhenPromiseIsResolved);
+                jest.runAllTimers();
+                expect(
+                    functionToCallWhenPromiseIsResolved
+                ).toHaveBeenCalledWith(1);
+            });
+        });
     });
 });
+
+/* Possible test cases:
+ * Resolve value asynchronously
+ * Reject
+ * Add a catch method
+ * Calling then() returns a promise
+ * Chaining calls
+ *      multiple then()
+ *      then(), catch()
+ *      then(), catch(), then()
+ * Reject if error occurs in executor
+ */
