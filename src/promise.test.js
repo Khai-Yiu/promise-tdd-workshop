1be7e7f473cb66bf4361513ce3efc5fadfdd3896
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-18 15:40:33 +0800

    REFACTOR: Describe structure

--- src/promise.test.js
+++ src/promise.test.js
@@ -1,5 +1,4 @@
 import MyPromise from './promise';
-jest.useFakeTimers();
 jest.spyOn(global, 'setTimeout');
 
 describe('MyPromise', () => {
@@ -11,56 +10,58 @@ describe('MyPromise', () => {
 
         expect(typeof myPromise.then).toBe('function');
     });
-    describe('given a promise that resolves immediately', () => {
-        it('should resolve with a value', () => {
-            const executor = (resolveFunction) => {
-                resolveFunction(1);
-            };
-            const myPromise = new MyPromise(executor);
-            const functionToCallWhenPromiseIsResolved = jest.fn();
-            myPromise.then(functionToCallWhenPromiseIsResolved);
-
-            expect(functionToCallWhenPromiseIsResolved).toHaveBeenCalledWith(1);
-        });
-    });
-    describe('given a promise that resolves after some time', () => {
-        beforeAll(() => {
-            jest.useFakeTimers();
-        });
-
-        afterAll(() => {
-            jest.useRealTimers();
-        });
-        describe('and the callback is registered before it resolves', () => {
-            it('calls the registered callback when it resolves', () => {
+    describe('given a promise that resolves', () => {
+        describe('immediately', () => {
+            it('should resolve with a value', () => {
                 const executor = (resolveFunction) => {
-                    setTimeout(() => {
-                        resolveFunction(1);
-                    }, 1000);
+                    resolveFunction(1);
                 };
                 const myPromise = new MyPromise(executor);
                 const functionToCallWhenPromiseIsResolved = jest.fn();
                 myPromise.then(functionToCallWhenPromiseIsResolved);
-                jest.runAllTimers();
+
                 expect(
                     functionToCallWhenPromiseIsResolved
                 ).toHaveBeenCalledWith(1);
             });
         });
-        describe('and the callback is registered after it resolves', () => {
-            it('calls the registered callback immediately', () => {
-                const executor = (resolveFunction) => {
-                    setTimeout(() => {
-                        resolveFunction(1);
-                    }, 10000);
-                };
-                const myPromise = new MyPromise(executor);
-                const functionToCallWhenPromiseIsResolved = jest.fn();
-                jest.runAllTimers();
-                myPromise.then(functionToCallWhenPromiseIsResolved);
-                expect(
-                    functionToCallWhenPromiseIsResolved
-                ).toHaveBeenCalledWith(1);
+        describe('after some time', () => {
+            beforeAll(() => {
+                jest.useFakeTimers();
+            });
+
+            afterAll(() => {
+                jest.useRealTimers();
+            });
+            describe('and the callback is registered before it resolves', () => {
+                it('calls the registered callback when it resolves', () => {
+                    const executor = (resolveFunction) => {
+                        setTimeout(() => {
+                            resolveFunction(1);
+                        }, 1000);
+                    };
+                    const myPromise = new MyPromise(executor);
+                    const functionToCallWhenPromiseIsResolved = jest.fn();
+                    myPromise.then(functionToCallWhenPromiseIsResolved);
+                    jest.runAllTimers();
+                    expect(
+                        functionToCallWhenPromiseIsResolved
+                    ).toHaveBeenCalledWith(1);
+                });
+                it('calls the registered callback immediately', () => {
+                    const executor = (resolveFunction) => {
+                        setTimeout(() => {
+                            resolveFunction(1);
+                        }, 10000);
+                    };
+                    const myPromise = new MyPromise(executor);
+                    const functionToCallWhenPromiseIsResolved = jest.fn();
+                    jest.runAllTimers();
+                    myPromise.then(functionToCallWhenPromiseIsResolved);
+                    expect(
+                        functionToCallWhenPromiseIsResolved
+                    ).toHaveBeenCalledWith(1);
+                });
             });
         });
     });
