9569affadf3e5698b158c067e0c2a16d4478e819
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-03-20 16:14:02 +0800

    RED: MyPromise › given an array of promises passed to MyPromise.allSettled › resolves to an empty array given an empty array

--- src/promise.test.js
+++ src/promise.test.js
@@ -181,17 +181,42 @@ describe('MyPromise', () => {
             expect(newPromise).toBeInstanceOf(MyPromise);
             expect(callback).toHaveBeenCalledWith('Rejected');
         });
+        describe('where some promises settle at different times', () => {
+            beforeAll(() => {
+                jest.useFakeTimers();
+            });
+
+            afterAll(() => {
+                jest.useRealTimers();
+            });
+
+            it('resolves to an array of results matching the order of promises in the array', () => {
+                const arrayOfPromises = [
+                    MyPromise.resolve(1),
+                    new MyPromise((resolve) => {
+                        setTimeout(() => resolve(2), 1000);
+                    }),
+                    MyPromise.resolve(3)
+                ];
+                const callback = jest.fn();
+                const newPromise = MyPromise.all(arrayOfPromises);
+                newPromise.then(callback);
+                jest.runAllTimers();
+
+                expect(newPromise).toBeInstanceOf(MyPromise);
+                expect(callback).toHaveBeenCalledWith([1, 2, 3]);
+            });
+        });
+    });
+    describe('given an array of promises passed to MyPromise.allSettled', () => {
+        it('resolves to an empty array given an empty array', () => {
+            const arrayOfPromises = [];
+            const callback = jest.fn();
+            const newPromise = MyPromise.allSettled(arrayOfPromises);
+            newPromise.then(callback);
+
+            expect(newPromise).toBeInstanceOf(MyPromise);
+            expect(callback).toHaveBeenCalledWith([]);
+        });
     });
 });
-
-/* Possible test cases:
- * Resolve value asynchronously
- * Reject
- * Add a catch method
- * Calling then() returns a promise
- * Chaining calls
- *      multiple then()
- *      then(), catch()
- *      then(), catch(), then()
- * Reject if error occurs in executor
- */
